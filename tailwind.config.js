0b1d9340eb565881e2aa29c716cf26ac70a16421
Greatawakening44 <trustgodmcjolly@gmail.com>
2026-06-28 20:55:50 +0100

    first-commit

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+
+  plugins: [
+  function ({ addUtilities }) {
+    addUtilities({
+      '.scrollbar-hide': {
+        '-ms-overflow-style': 'none',
+        'scrollbar-width': 'none',
+        '&::-webkit-scrollbar': { display: 'none' },
+      },
+    });
+  },
+],
+
+}
+
+
