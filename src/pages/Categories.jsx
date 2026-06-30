f84da54c46f58de9880576ab28723f96e80b1c43
Greatawakening44 <trustgodmcjolly@gmail.com>
2026-06-30 02:41:46 +0100

    menr

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -8,15 +8,15 @@ const MoCategories = () => {
 
 
     return (
-        <div className='min-h-screen bg-white px-4 sm:px-6 md:hidden mt-3'>
+        <div className='min-h-screen bg-white px-4 sm:px-6 md:hidden mt-3 border-b'>
             {categories.map((category) => (
                 <button
                 key={category}
                 onClick={() => setActive(category)}
-                className={` py-2 px-5 text-sm font-medium transition-colors duration-200 
+                className={` py-2 px-5 text-sm font-medium transition-colors duration-600 
                     cursor-pointer border-b-2
                     ${active === category
-                        ? 'border-red-600 text-red-600' 
+                        ? 'border-[#5E0202] text-[#5E0202]' 
                         : 'border-transparent text-gray-500 hover:text-black'}`
                 }
                 >{category}</button>
